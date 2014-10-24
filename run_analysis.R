1b3f2615d10ca41bcb27f9d04dde84343adcb9c1
Martin Tapp <martin.d.tapp@gmail.com>
2014-10-24 16:09:10 -0400

    Update run_analysis.R
    Corrected typo

--- run_analysis.R
+++ run_analysis.R
@@ -7,7 +7,7 @@ require("reshape2")
 # 2. Extract only the measurements on the mean and standard deviation for each measurement
 # 3. Use descriptive activity names to name the activities in the data set
 # 4. Appropriately label the data set with descriptive variable names
-# 5. Creates an independent tidy data set with the average of each variable for each activity and each subject
+# 5. Create an independent tidy data set with the average of each variable for each activity and each subject
 
 # Download the input file if not available locally
 # >>> See Step 0.
